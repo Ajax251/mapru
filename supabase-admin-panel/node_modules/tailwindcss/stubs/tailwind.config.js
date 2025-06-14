5cf758a548a6ce92be2a82ce30e2aed6896b6807
Ajax251 <marat2008@gmail.com>
2025-06-14 16:34:32 +0300

    upd

---
+++ supabase-admin-panel/node_modules/tailwindcss/stubs/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Указываем, где искать классы
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
