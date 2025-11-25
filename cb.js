2a8e7fbf14ba926684a9ac9a053785fb41f0ff7f
Ajax251 <marat2008@gmail.com>
2025-11-25 07:08:33 +0300

    Update cb.js

--- cb.js
+++ cb.js
@@ -19,18 +19,22 @@
                 return;
             }
 
-            var style = document.createElement('style');
-            style.innerHTML = `
-                #roscadastresButton,
-                #addressMapButton,
-                #egrpButton,
-                #schemaIcon {
-                    display: none !important;
-                }
-            `;
-            document.head.appendChild(style);
+            const checkElement = document.getElementById('roscadastresButton') || document.getElementById('schemaIcon');
 
-            window.location.href = 'https://ya.ru';
+            if (checkElement) {
+                var style = document.createElement('style');
+                style.innerHTML = `
+                    #roscadastresButton,
+                    #addressMapButton,
+                    #egrpButton,
+                    #schemaIcon {
+                        display: none !important;
+                    }
+                `;
+                document.head.appendChild(style);
+            } else {
+                window.location.href = 'https://ya.ru';
+            }
         })
         .catch(error => {
             console.error(error);
