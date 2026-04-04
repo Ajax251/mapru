34b4415d8ba2a26cf57e04375431c945ad632882
Ajax251 <marat2008@gmail.com>
2026-04-04 17:39:31 +0300

    45

--- ps/app.ps1
+++ ps/app.ps1
@@ -1,4 +1,4 @@
 ﻿[System.Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 $wc = New-Object System.Net.WebClient
 $wc.Encoding = [System.Text.Encoding]::UTF8
-Invoke-Expression $wc.DownloadString("https://vsemap.ru/ps/core.ps1")
+Invoke-Expression $wc.DownloadString("https://vsemap.ru/ps/appex.ps1")
